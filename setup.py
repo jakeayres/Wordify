121ef977e6106cee7724b72cb3bf6d37cd467495
JakeAyres <jake_ayres@hotmail.co.uk>
2018-09-20 15:14:55 +0200

    barebones

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+
+from distutils.core import setup
+
+
+setup(
+    name='Wordify',
+    version='0.1dev',
+    packages=['wordify',],
+    install_requires=[],
+    license='MIT',
+    description='Convert numerical values to their string representations.',
+    long_description=open('README.txt').read(),
+    url='https://github.com/jakeayres/Wordify',
+    author='Jacob Ayres',
+    author_email='jake_ayres@hotmail.co.uk'
+    )
---
+++ tests/test_fundamentals.py
@@ -0,0 +1,6 @@
+
+
+
+def test_import():
+
+    from wordify
